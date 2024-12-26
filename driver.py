706fa6749edda097a17a343254a98771d50da609
Tengyu Chen <chenty0514@gmail.com>
2024-12-26 19:01:22 +0800

    Add locator.yml to manage element xpath

--- driver.py
+++ driver.py
@@ -1,5 +1,50 @@
+from selenium.common.exceptions import NoSuchElementException
 from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import yaml
+
+
+locator_dict = dict()
+locator_order = [By.ID, By.XPATH, By.CSS_SELECTOR]
+
+
+class LocatorKey:
+    # login
+    JOIN_BUTTON = "join_button"
+
+    # conversation
+    CONVERSATION = "conversation"
+    TAG_TO_BE_SENT = "tag_to_be_sent"
+
+    # message
+    MESSAGE_BOX = "message_box"
+    MESSAGE_LIST = "message_list"
+    CARD_MSG_TITLE = "card_msg_title"
+    TEXT_MSG_RECV = "text_msg_recv"
+    TEXT_MSG_SEND = "text_msg_send"
+
+    # input
+    TEXTAREA = "textarea"
+    SEND_BUTTON = "send_button"
+
+
+def load_config(file_path):
+    try:
+        with open(file_path, "r") as file:
+            global locator_dict
+            config = yaml.safe_load(file)
+            for key, value in config.items():
+                keys = key.split(".")
+                d = locator_dict
+                for k in keys[:-1]:
+                    d = d.setdefault(k, {})
+                d[keys[-1]] = value
+
+    except FileNotFoundError:
+        print("Configuration file not found")
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file: {e}")
 
 
 def init_driver():
@@ -8,10 +53,41 @@ def init_driver():
     # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--window-size=x1080")
 
     # 配置 Selenium Wire 来捕获请求
     return webdriver.Chrome(
         options=chrome_options,
         seleniumwire_options={"verify_ssl": False},
     )
+
+
+def find_elements(driver, key):
+    if key not in locator_dict:
+        return []
+
+    locators = locator_dict[key]
+    by = list(locators.keys())[0]
+    value = locators[by]
+    return driver.find_elements(by, value)
+
+
+def find_element(driver, key):
+    try:
+        if key not in locator_dict:
+            return None
+
+        locators = locator_dict[key]
+        by = list(locators.keys())[0]
+        value = locators[by]
+        return driver.find_element(by, value)
+
+    except NoSuchElementException:
+        return None
+
+
+if __name__ == "__main__":
+    load_config("./locator.yml")
+    msg_list = locator_dict["message_list"]
+    print(list(msg_list.keys())[0])
+    print(next(iter(msg_list)))
--- goofish.py
+++ goofish.py
@@ -1,11 +1,35 @@
 import pickle
 import time
+from enum import Enum
+from driver import load_config, LocatorKey, find_element, find_elements
+
 from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys
+
+
+class SenderType(Enum):
+    BUYER = "buyer"
+    SELLER = "seller"
+
+
+class MessageType(Enum):
+    CARD = "card"
+    TEXT = "text"
+
+
+class ChatMessage:
+    def __init__(self, sender_type: SenderType, msg_type: MessageType, content: str):
+        self.sender_type = sender_type
+        self.msg_type = msg_type
+        self.content = content
+
+    def __repr__(self) -> str:
+        return f"ChatMessage[sender: {self.sender_type.value}, type: {self.msg_type.value}, content: {self.content}]"
 
 
 def init_driver():
@@ -33,97 +57,118 @@ def login_to_im(driver, cookie_file):
     # refresh page
     driver.refresh()
 
-    # wait for login box
-    login_box_id = "alibaba-login-box"
-    driver_wait = WebDriverWait(driver, 10)
-    driver_wait.until(EC.visibility_of_element_located((By.ID, login_box_id)))
+    try:
+        # wait for login box
+        login_box_id = "alibaba-login-box"
+        driver_wait = WebDriverWait(driver, 10)
+        driver_wait.until(EC.visibility_of_element_located((By.ID, login_box_id)))
 
-    # click join button
-    driver.switch_to.frame(login_box_id)
-    join_buttion = driver.find_element(By.XPATH, "//button[text()='快速进入']")
-    join_buttion.click()
-    driver.switch_to.default_content()
-    time.sleep(3)
-
-
-def switch_conversation(driver):
-    items = driver.find_elements(
-        By.XPATH,
-        "/html//div[@id='conv-list-scrollable']/div[@class='rc-virtual-list']//div[@class='rc-virtual-list-holder-inner']/div/div/div",
-    )
-    print(len(items))
-    items[2].click()
-    # item = driver.find_element(
-    #     By.XPATH,
-    #     "/html//div[@id='conv-list-scrollable']/div[@class='rc-virtual-list']/div[@class='rc-virtual-list-holder']//div[@class='rc-virtual-list-holder-inner']/div/div//div[@class='ant-dropdown-trigger']/div/div",
-    # )
-    # item.click()
-    time.sleep(2)
+        # click join button
+        driver.switch_to.frame(login_box_id)
+        join_buttion = find_element(driver, LocatorKey.JOIN_BUTTON)
+        if join_buttion:
+            join_buttion.click()
+            driver.switch_to.default_content()
+            print("Login to goofish im page success")
+            time.sleep(3)
+    except TimeoutException:
+        print("Waiting for login box timeout")
+        pass
 
 
-def check_paid(driver):
-    order_msgs = driver.find_elements(
-        By.XPATH,
-        "/html//div[@id='msg-list-container']/div[@class='ant-spin-nested-loading css-apn68']//ul[@class='ant-list-items']/div/li[@class='ant-list-item']//div[@class='msg-dx-title--BLWdPAHj']",
-    )
-    for msg in order_msgs:
-        print(msg.text)
+def is_wating_deliver(msg_list):
+    for msg in reversed(msg_list):
+        if (
+            msg.sender_type == SenderType.SELLER
+            and "https://pan.baidu.com" in msg.content
+            and "提取码" in msg.content
+        ):
+            return False
+        elif (
+            msg.sender_type == SenderType.BUYER
+            and "我已付款，等待你发货" in msg.content
+        ):
+            return True
+    return False
 
 
 def get_chat_message(driver):
-    chat_box = driver.find_element(By.ID, "message-list-scrollable")
-    last_top = driver.execute_script("return arguments[0].scrollTop", chat_box)
+    message_box = find_element(driver, LocatorKey.MESSAGE_BOX)
+    if message_box is None:
+        print("message_box is None")
+        return []
 
     # scroll to top
+    last_top = driver.execute_script("return arguments[0].scrollTop", message_box)
     for _ in range(10):
         driver.execute_script(
             "arguments[0].scrollTop = arguments[0].scrollTop - arguments[0].clientHeight",
-            chat_box,
+            message_box,
         )
 
-        new_top = driver.execute_script("return arguments[0].scrollTop", chat_box)
+        new_top = driver.execute_script("return arguments[0].scrollTop", message_box)
         if new_top == last_top:
             break
 
         last_top = new_top
 
     # return messages
-    return driver.find_elements(
-        By.XPATH,
-        "//div[contains(@class, 'message-text--zV88pB7N')]",
-    )
+    recent_messages = []
+    for element in find_elements(driver, LocatorKey.MESSAGE_LIST):
+        title = find_element(element, LocatorKey.CARD_MSG_TITLE)
+        if title:
+            msg = ChatMessage(SenderType.BUYER, MessageType.CARD, title.text)
+            recent_messages.append(msg)
+            continue
+
+        text_recv = find_element(element, LocatorKey.TEXT_MSG_RECV)
+        if text_recv:
+            msg = ChatMessage(SenderType.BUYER, MessageType.TEXT, text_recv.text)
+            recent_messages.append(msg)
+            continue
+
+        text_send = find_element(element, LocatorKey.TEXT_MSG_SEND)
+        if text_send:
+            msg = ChatMessage(SenderType.SELLER, MessageType.TEXT, text_send.text)
+            recent_messages.append(msg)
+            continue
+
+    return recent_messages
 
 
 def send_message(driver, text):
     # input text
-    textarea = driver.find_element(
-        By.XPATH,
-        "/html//div[@id='content']//div[@class='ant-layout ant-layout-has-sider chat-container--gftCko_u css-apn68']//textarea[@placeholder='请输入消息，按Enter键发送或点击发送按钮发送']",
-    )
-    textarea.send_keys(Keys.TAB)
-    textarea.clear()
-    textarea.send_keys(text)
+    textarea = find_element(driver, LocatorKey.TEXTAREA)
+    if textarea:
+        textarea.send_keys(Keys.TAB)
+        textarea.clear()
+        textarea.send_keys(text)
 
-    # click send
-    send_button = driver.find_element(
-        By.XPATH,
-        "/html//div[@id='content']//div[@class='ant-layout ant-layout-has-sider chat-container--gftCko_u css-apn68']//div[@class='sendbox-bottom--O2c5fyIe']/button[@type='button']",
-    )
-    send_button.click()
+        # click send
+        send_button = find_element(driver, LocatorKey.SEND_BUTTON)
+        if send_button:
+            send_button.click()
 
 
-def start_loop(driver):
-    pass
+def start_auto_deliver(driver):
+    while True:
+        time.sleep(5)
+        for conversation in find_elements(driver, LocatorKey.CONVERSATION):
+            tag = find_element(conversation, LocatorKey.TAG_TO_BE_SENT)
+            if tag and tag.text == "等待卖家发货":
+                conversation.click()
+                time.sleep(2)
+                print("Found conversation to deliver")
+                msg_list = get_chat_message(driver)
+                if is_wating_deliver(msg_list):
+                    send_message(driver, "hello")
 
 
 if __name__ == "__main__":
-    driver = init_driver()
-    login_to_im(driver, "./cookies/goofish_cookies.pkl")
-    switch_conversation(driver)
-    messages = get_chat_message(driver)
-    for msg in messages:
-        print(msg.text)
+    config_file = "./locator.yml"
+    cookie_file = "./cookies/goofish_cookies.pkl"
 
-    check_paid(driver)
-    # send_message(driver, "hello")
-    time.sleep(100)
+    load_config(config_file)
+    driver = init_driver()
+    login_to_im(driver, cookie_file)
+    start_auto_deliver(driver)
